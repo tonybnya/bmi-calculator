e52b2d6273351c8f227608f62da84e94696bfdbb
tonybnya <nya.tony2010@gmail.com>
2025-09-18 17:44:39 +0100

    feat: define the BMICategory model

--- backend/bmi_app/models.py
+++ backend/bmi_app/models.py
@@ -5,7 +5,7 @@ Author      : @tonybnya
 """
 from datetime import datetime, timezone
 
-from sqlalchemy import Column, DateTime, Integer, String
+from sqlalchemy import Column, DateTime, Float, Integer, String
 from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()
@@ -30,3 +30,17 @@ class User(Base):
 
     # Relationship to measurements
     measurements = relationship("Measurement", back_populates="user")
+
+
+class BMICategory(Base):
+    """
+    Define the BMI Category model.
+    """
+    __tablename__ == "bmi_categories"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(50), nullable=False)
+    min_value = Column(Float, nullable=True)
+    max_value = Column(Float, nullable=True)
+
+    measurements = relationship("Measurement", back_populates="category")
