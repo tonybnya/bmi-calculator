c9a9bdff8f1d769aefa99711c0b16fb989a0c210
tonybnya <nya.tony2010@gmail.com>
2025-09-18 19:22:40 +0100

    perf: add helper functions for units conversion

--- backend/bmi_app/core/utils.py
+++ backend/bmi_app/core/utils.py
@@ -3,3 +3,48 @@ Script Name : core.py
 Description : Helper functions
 Author      : @tonybnya
 """
+
+
+def categorize(bmi: float) -> str:
+    """
+    Define the category of a BMI measure.
+    """
+    if bmi < 18.5:
+        return "Underweight"
+    if bmi < 25:
+        return "Normal"
+    if bmi < 30:
+        return "Overweight"
+    if bmi < 35:
+        return "Obesity I"
+    if bmi < 40:
+        return "Obesity II"
+    # bmi above 40
+    return "Obesity III"
+
+
+def to_kg(weight: float, unit: str) -> float:
+    """
+    Convert a given weight to kilograms.
+    """
+    # kilograms -> kilograms
+    if unit == "kg":
+        return weight
+    # lb (pounds) -> kg
+    return weight * 0.45359237
+
+
+def to_meters(height: float, unit: str) -> float:
+    """
+    Convert a given height to meters.
+    """
+    # meters -> meters
+    if unit == "m":
+        return height
+
+    # centimeters -> meters
+    if unit == "cm":
+        return height / 100.0
+
+    # in/ft(inches/feet) -> meters
+    return height * 0.0254
