d5c82efc91049510ab541ede27c810d49557ef11
tonybnya <nya.tony2010@gmail.com>
2025-09-18 17:39:14 +0100

    feat: define the User model

--- backend/bmi_app/models.py
+++ backend/bmi_app/models.py
@@ -1,5 +1,32 @@
 """
-Script Name : main.py
+Script Name : models.py
 Description : SQLAlchemy models (User, Measurement, BMICategory)
 Author      : @tonybnya
 """
+from datetime import datetime, timezone
+
+from sqlalchemy import Column, DateTime, Integer, String
+from sqlalchemy.orm import declarative_base, relationship
+
+Base = declarative_base()
+
+
+class User(Base):
+    """
+    Define the User model.
+    """
+    __tablename__ == "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(50), unique=True, nullable=False)
+    email = Column(String(100), unique=True, nullable=False)
+    password_hash = Column(String(255), nullable=False)
+    created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
+    updated_at = Column(
+        DateTime,
+        default=lambda: datetime.now(timezone.utc),
+        onupdate=lambda: datetime.now(timezone.utc)
+    )
+
+    # Relationship to measurements
+    measurements = relationship("Measurement", back_populates="user")
